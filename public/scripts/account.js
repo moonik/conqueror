9048f50499388f96b5c4953db8ade1d29d0ad2c6
Roman <romanveos@gmail.com>
2017-03-23 13:02:42 +0200

    fixes in front end

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -12,24 +12,6 @@ angular.module('conquerorApp').controller('MainCtrl', function ($scope, $rootSco
         })
     };
 
-    $scope.signUp = function () {
-        $http.post('api/users', $scope.userForm).then(function (response) {
-         $window.location.href = "#/login"
-            console.log('signed up');
-            alert("Registration completed, you can log in now");
-        });
-    };
-
-    $scope.signIn = function () {
-        $http.post('api/login', $scope.userForm).then(function (response) {
-            localStorage.setItem('jwt', response.headers()['x-auth-token']);
-            $scope.getUser();
-            console.log('signed in');
-            $window.location.href = "#/home"
-        })
-         $scope.userForm = {};
-    };
-
     $scope.signOut = function () {
         $rootScope.isSignedIn = false;
         localStorage.removeItem('jwt');
