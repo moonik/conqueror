716c1037c45effd5cd3684d95bbb43bee3f27825
Roman <romanveos@gmail.com>
2017-03-28 00:59:00 +0300

    fixed registration and login

--- public/scripts/registration.js
+++ public/scripts/registration.js
@@ -7,26 +7,21 @@ angular.module('conquerorApp').controller('RegistrationCtrl', function ($scope, 
      $scope.signUp = function () {
         $http.post('api/users', $scope.userForm).then(function (response) {
             console.log('signed up');
+            $http.post('api/login', $scope.userForm).then(function (response) {
+                localStorage.setItem('jwt', response.headers()['x-auth-token']);
+                $scope.getUser();
+                console.log('signed in');
+                $http.get('api/castle/checkCastle').then(function (response) {
+                    $rootScope.userCastle = true;
+                    },function(response){
+                        $rootScope.userCastle = false;
+                        closeModal(undefined);
+                        });
+                })
             $rootScope.isSignedUp = true;
             },function(response){
                 $rootScope.isSignedUp = false;
                 });
-        $scope.userForm = {};
-        };
-
-     $scope.signIn = function () {
-        $http.post('api/login', $scope.userForm).then(function (response) {
-            localStorage.setItem('jwt', response.headers()['x-auth-token']);
-            $scope.getUser();
-            console.log('signed in');
-            $http.get('api/castle/checkCastle').then(function (response) {
-                $rootScope.userCastle = true;
-                },function(response){
-                    $rootScope.userCastle = false;
-                    closeModal(undefined);
-                    });
-            })
-        $scope.userForm = {};
         };
 
      $rootScope.getUser = function () {
